4d1dd06fef40b5bc958178b8e990f3538a0a3bd7
HimanshuRanka <himanshuranka2612@gmail.com>
2021-12-17 01:22:09 -0500

    added data files

--- scrape_one_look.py
+++ scrape_one_look.py
@@ -4,27 +4,28 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-URL_BASE = "https://www.onelook.com/thesaurus/?s="
-sentence = "riding on top of waves".replace(" ", "%20")
-print(sentence)
 
-browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-browser.get(URL_BASE + sentence)
-time.sleep(0.55)
+def get_words(sentence):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    URL_BASE = "https://www.onelook.com/thesaurus/?s="
+    sentence = sentence.replace(" ", "%20")
+    print(sentence)
 
-results_html = browser.page_source
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    browser.get(URL_BASE + sentence)
+    time.sleep(0.55)
 
-# print(results_html)
+    results_html = browser.page_source
 
-soup = bs4.BeautifulSoup(results_html, "html.parser")
-results = []
-result_section = soup.find("div", {"id": "zone1"})
-result_collection = result_section.findChildren("div", {"class": "res ressyn"})
-for i in range(1, 11):
-    result = soup.find("div", {"resid": str(i)}).attrs["thesw"].replace("%20", " ")
-    results.append(result)
+    # print(results_html)
 
-print(results)
+    soup = bs4.BeautifulSoup(results_html, "html.parser")
+    results = []
+    for i in range(1, 11):
+        result = soup.find("div", {"resid": str(i)}).attrs["thesw"].replace("%20", " ")
+        results.append(result)
+
+    print(results)
+    return results
 
